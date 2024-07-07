ac5ba72dbe9995edeb1597295fe4096f8b8b3d06
Claire West <clairewest@Claires-MBP-M1.home>
2024-07-07 13:04:01 -0500

    still broken

--- coupled_dipole_model.py
+++ coupled_dipole_model.py
@@ -1,27 +1,27 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.misc import derivative
-from scipy.special import spherical_jn
-from scipy.special import spherical_yn
-    
-e = 4.80326E-10 # elementary charge, [statC, g^1/2 cm^3/2 s^-1]
-c = 2.998E+10 # speed of light [cm/s]
-hbar_eVs = 6.58212E-16 # Planck's constant [eV*s]
+from scipy.special import spherical_jn as sph_jn
+from scipy.special import spherical_yn as sph_yn
+
+e = 4.80326E-10  # elementary charge, [statC, g^1/2 cm^3/2 s^-1]
+c = 2.998E+10  # speed of light [cm/s]
+hbar_eVs = 6.58212E-16  # Planck's constant [eV*s]
+
 
 class Sphere_Polarizability:
-    def __init__(self, centers, all_radii, n, wp, eps_inf, gam_drude, shell=False, shell_m=False):
+    """ Defines polarizability of spherical particles using Mie.
+    """
+
+    def __init__(self,
+                 all_radii,  # radii of the spheres [cm]
+                 n,  # refractive index of background [unitless]
+                 wp,  # wp -- bulk plasma frequency [1/s]
+                 eps_inf,  # response of ionic background [unitless]
+                 gam_drude,  # Drude damping (quasistatic, nonrad.) [1/s]
+                 shell=0,
+                 shell_m=0):
         """Defines the different system parameters.
-        
-        Keyword arguments:
-        centers -- coordinates of centers of each particle [cm]
-        all_radii -- radii of the oscillators [cm]
-        n -- [unitless] refractive index of background 
-        wp -- [1/s] bulk plasma frequency
-        eps_inf -- [unitless] static dielectric response of ionic background 
-        gamNR_qs -- [1/s] Drude damping (i.e. quasistatic, nonradiative)
         """
-        self.centers = centers
+
         self.all_radii = all_radii
         self.n = n
         self.wp = wp
@@ -31,183 +31,164 @@ class Sphere_Polarizability:
         self.shell_m = shell_m
 
     def psi(self, rho):
-        return rho*spherical_jn(1, rho)
+        return rho * sph_jn(1, rho)
 
-    def psi_prime(self, rho):
-        return spherical_jn(1, rho) + rho*spherical_jn(1, rho, derivative=True)
+    def psi_p(self, rho):
+        return sph_jn(1, rho) + rho * sph_jn(1, rho, derivative=True)
 
     def hankel(self, rho):
-        return spherical_jn(1, rho) + 1j*spherical_yn(1, rho)
+        return sph_jn(1, rho) + 1j * sph_yn(1, rho)
 
-    def hankel_prime(self, rho):
-        return spherical_jn(1, rho, derivative=True) + 1j*spherical_yn(1, rho,derivative=True)
+    def hankel_p(self, rho):
+        return (sph_jn(1, rho, derivative=True)
+                + 1j * sph_yn(1, rho, derivative=True))
 
     def xi(self, rho):
-        return rho*self.hankel(rho)
+        return rho * self.hankel(rho)
 
-    def xi_prime(self, rho):
-        return self.hankel(rho) + rho*self.hankel_prime(rho)
+    def xi_p(self, rho):
+        return self.hankel(rho) + rho * self.hankel_p(rho)
+
+    def optical_params(self, w, radius):
+        eps = self.eps_inf - self.wp**2 / (w**2 + 1j * w * self.gamNR_qs)
+        m1 = np.sqrt(eps)
+        m2 = self.shell_m
+        k = w / c * self.n
+        x = k * radius
+        y = k * (radius + self.shell)
+        return m1, m2, k, x, y
 
     def mie_coefficents(self, w, radius):
-        eps = self.eps_inf - self.wp**2/(w**2 + 1j*w*self.gamNR_qs)
-        m = np.sqrt(eps)
-        k = w/c*self.n
-        x = k*radius
-        numer_a = m*self.psi(m*x)*self.psi_prime(x) - self.psi(x)*self.psi_prime(m*x)
-        denom_a = m*self.psi(m*x)*self.xi_prime(x) - self.xi(x)*self.psi_prime(m*x)
-        numer_b = self.psi(m*x)*self.psi_prime(x) - m*self.psi(x)*self.psi_prime(m*x)
-        denom_b = self.psi(m*x)*self.xi_prime(x) - m*self.xi(x)*self.psi_prime(m*x)
-        a = numer_a/denom_a
-        b = numer_b/denom_b
-        return a 
+        m, _, k, x, _ = self.optical_params(w, radius)
+        numer_a = (m * self.psi(m * x) * self.psi_p(x)
+                   - self.psi(x) * self.psi_p(m * x))
+        denom_a = (m * self.psi(m * x) * self.xi_p(x)
+                   - self.xi(x) * self.psi_p(m * x))
+        numer_b = (self.psi(m * x) * self.psi_p(x)
+                   - m * self.psi(x) * self.psi_p(m * x))
+        denom_b = (self.psi(m * x) * self.xi_p(x)
+                   - m * self.xi(x) * self.psi_p(m * x))
+        a = numer_a / denom_a
+        b = numer_b / denom_b
+        return a, b
 
-    def chi(self,z):
-        return -z*spherical_yn(1, z)
+    def chi(self, z):
+        return -z * sph_yn(1, z)
 
-    def chi_prime(self,z):
-        return -spherical_yn(1, z) + -z*spherical_yn(1, z, derivative=True)
-
+    def chi_p(self, z):
+        return -sph_yn(1, z) + -z * sph_yn(1, z, derivative=True)
 
     def mie_coeff_coated_sphere(self, w, radius):
-        eps1 = self.eps_inf - self.wp**2/(w**2 + 1j*w*self.gamNR_qs)
-        m1 = np.sqrt(eps1)
-        m2 = self.shell_m
-        k = w/c*self.n
-        x = k*radius
-        y = k*(radius+self.shell)
-        A = ( m2*self.psi(m2*x)*self.psi_prime(m1*x) - m1*self.psi_prime(m2*x)*self.psi(m1*x) ) /\
-            ( m2*self.chi(m2*x)*self.psi_prime(m1*x) - m1*self.chi_prime(m2*x)*self.psi(m1*x) )
-        a = ( self.psi(y)*( self.psi_prime(m2*y) - A*self.chi_prime(m2*y) ) -  m2*self.psi_prime(y)*( self.psi(m2*y) - A*self.chi(m2*y) ) ) / \
-            ( self.xi(y)* ( self.psi_prime(m2*y) - A*self.chi_prime(m2*y) ) -  m2*self.xi_prime(y)* ( self.psi(m2*y) - A*self.chi(m2*y) ) )
-        return a 
+        m1, m2, k, x, y = self.optical_params(w, radius)
+        A_num = (m2 * self.psi(m2 * x) * self.psi_p(m1 * x)
+                 - m1 * self.psi_p(m2 * x) * self.psi(m1 * x))
+        A_denom = (m2 * self.chi(m2 * x) * self.psi_p(m1 * x)
+                   - m1 * self.chi_p(m2 * x) * self.psi(m1 * x))
+        A = A_num / A_denom
+        a_num = (self.psi(y) * (self.psi_p(m2 * y) - A * self.chi_p(m2 * y))
+                 - (m2 * self.psi_p(y)
+                     * (self.psi(m2 * y) - A * self.chi(m2 * y))))
 
+        a_den = ((self.xi(y) * (self.psi_p(m2 * y) - A * self.chi_p(m2 * y))
+                  - (m2 * self.xi_p(y)
+                     * (self.psi(m2 * y) - A * self.chi(m2 * y)))))
+        return a_num / a_den
 
-    def alpha(self, w, radius): 
-        k = w/c*self.n
-        if self.shell == False:
-            a = self.mie_coefficents(w=w,radius=radius)
-        if self.shell != False: 
+    def alpha(self, w, radius):
+        """ Polarizability [cm^3]
+        """
+        k = w / c * self.n
+        if self.shell == 0:
+            a, _ = self.mie_coefficents(w=w, radius=radius)
+        if self.shell != 0:
             a = self.mie_coeff_coated_sphere(w=w, radius=radius)
-        alpha = 3/(2.*k**3)*1j*(a)#+b) 
+        alpha = 3 / (2. * k**3) * 1j * a
         return alpha
+
 
 class CrossSections:
-    def __init__(self, 
-        centers, 
-        all_radii,
-        w, 
-        num, 
-        n, 
-        wp, 
-        eps_inf, 
-        gam_drude,
-        shell=False,
-        shell_m=False,
-        ):
-        """Defines the different system parameters.
-        
-        Keyword arguments:
-        centers -- coordinates of centers of each particle [cm]
-        all_radii -- radii of the oscillators [cm]
-        w -- array of frequencies [1/s]
-        num -- number of particles 
-        n -- [unitless] refractive index of background 
-        wp -- [1/s] bulk plasma frequency
-        gamNR_qs -- [1/s] Drude damping (i.e. quasistatic, nonradiative)
-        """
-        self.centers = centers
+    def __init__(self,
+                 alphas,
+                 centers,
+                 all_radii,
+                 unit_vecs,
+                 w,
+                 n,
+                 ):
+        self.alphas = alphas
+        self.centers = centers  # [num_part, dim]
         self.all_radii = all_radii
-        self.w = w
-        self.num = num
+        self.unit_vecs = unit_vecs
+        self.w = w  # [1/s]
+        self.num = len(alphas)  # number of particles
         self.n = n
-        self.wp = wp
-        self.eps_inf = eps_inf
-        self.gamNR_qs = gam_drude
-        self.shell = shell
-        self.shell_m = shell_m
-        self.dip_params = Sphere_Polarizability(self.centers, self.all_radii, self.n, self.wp, self.eps_inf, self.gamNR_qs, self.shell, self.shell_m)
-        self.num_radii_combs = self.centers.shape[0]
-        self.num_gap = self.centers.shape[1]
-        self.num_freq = self.centers.shape[2]
-
-    def delta_mn(self,m,n):
-        if m == n: 
-            return 1
-        else: 
-            return 0
-
-    def A_ij(self, dip_i, dip_j):
-        """ Off diagonal block terms in A_tilde. 
-        """
-        k = self.w/c*self.n
-        A_ij = np.zeros( (self.num_radii_combs, self.num_gap, self.num_freq, 3, 3) ,dtype=complex) 
-        r = self.centers[:,:,:,dip_i,:] - self.centers[:,:,:,dip_j,:] # [cm], distance between ith and jth particle 
-        magr = np.sqrt(r[:,:,:,0]**2 + r[:,:,:,1]**2 + r[:,:,:,2]**2)
-        for n in range(0,3):
-            for m in range(0,3):
-                A_ij[:,:,:,n,m] = np.exp(1j*k*magr) / magr**3 *(
-                           k**2*( r[:,:,:,n]*r[:,:,:,m] - magr**2*self.delta_mn(n+1,m+1) ) + 
-                            (1-1j*k*magr) / magr**2 *(magr**2*self.delta_mn(n+1,m+1)-3*r[:,:,:,n]*r[:,:,:,m]) 
-                         ) 
-        return A_ij
-
-    def alpha_tensor(self, dip_i):
-        """ Polarizability tensor in the cartesian basis of the single particle.
-            This assumes the sphere has an isotropic polarizability. 
-        """
-        alpha = np.zeros( (self.num_radii_combs, self.num_gap, self.num_freq, 3, 3) ,dtype=complex)
-        alpha[:,:,:,0,0] = self.dip_params.alpha(w=self.w, radius=self.all_radii[:,:,:,dip_i])
-        alpha[:,:,:,1,1] = self.dip_params.alpha(w=self.w, radius=self.all_radii[:,:,:,dip_i])
-        alpha[:,:,:,2,2] = self.dip_params.alpha(w=self.w, radius=self.all_radii[:,:,:,dip_i])
-        return alpha
+        self.k = self.w / c * self.n  # [1/cm]
 
     def A_ii(self, dip_i, dip_j):
-        """On diagonal block terms in A_tilde.
+        """On diagonal vectors of A_matrix.
+           Alpha^-1 p_hat
         """
-        alpha = self.alpha_tensor(dip_i=dip_i)
-        A_ii = np.linalg.inv(alpha)
+        A_ii = self.alphas[dip_i]**(-1)
+        # print("{:.1e}".format(A_ii))
         return A_ii
 
-    def A_tilde(self):
-        """A_tilde = [num_freq, 3*N, 3*N]
+    def A_ij(self, dip_i, dip_j):
+        """Off diagonal vectors of A_matrix
         """
-        A_tilde = np.zeros( (self.num_radii_combs, self.num_gap, self.num_freq, 3*self.num, 3*self.num) ,dtype=complex) 
-        for i in range(0 , self.num): 
-            for j in range(0, self.num):
-                if i == j:  
-                    A_tilde[:, :, :, 3*i : 3*(i+1), 3*i : 3*(i+1)] = self.A_ii(dip_i=i, dip_j=i)
-                if i != j:
-                    A_tilde[:, :, :, 3*i : 3*(i+1), 3*j : 3*(j+1)] = self.A_ij(dip_i=i, dip_j=j)
-        return A_tilde
+        r_ij = self.centers[dip_i, :] - self.centers[dip_j, :]
+        phat_i = self.unit_vecs[dip_i, :]
+        phat_j = self.unit_vecs[dip_j, :]
+        magr_ij = np.linalg.norm(r_ij)
+        if magr_ij == 0:
+            A_ij = 0
+            return A_ij
 
-    def P_tilde(self, drive):
-        E0_tilde = np.zeros((self.num_radii_combs, self.num_gap, self.num_freq, 3*self.num, 1 ) )
-        P_tilde = np.zeros(( self.num_radii_combs, self.num_gap, self.num_freq, 3*self.num, 1 ),dtype=complex)
+        rhat_ij = r_ij / magr_ij
+        phat_i_dot_nn_dot_phat_j = (np.dot(phat_i, rhat_ij)
+                                    * np.dot(rhat_ij, phat_j))
+        near = ((3 * phat_i_dot_nn_dot_phat_j - np.dot(phat_i, phat_j))
+                / magr_ij**3)
+        intermed = 0#(-1j * self.k
+                    #* (3 * phat_i_dot_nn_dot_phat_j - np.dot(phat_i, phat_j))
+                    #/ magr_ij**2)
+        far = 0#(self.k**2
+               #* (phat_i_dot_nn_dot_phat_j - np.dot(phat_i, phat_j))
+               #/ magr_ij)
+        # print("{:.1e}, {:.1e}, {:.1e}".format(near, intermed, far))
+        # print()
+        A_ij = np.exp(1j * self.k * magr_ij) * (near + intermed + far)
+        return A_ij
+
+    def A_Matrix(self):
+        """A_Matrix = [N, N]
+        """
+        A_Matrix = np.zeros((self.num,
+                             self.num),
+                            dtype=complex)
         for i in range(0, self.num):
-            E0_tilde[:, :, :, 3*i,:] = drive[0]
-            E0_tilde[:, :, :, 3*i+1,:] = drive[1]
-            E0_tilde[:, :, :, 3*i+2,:] = drive[2]
-        P_tilde = np.linalg.solve(self.A_tilde(), E0_tilde)
-        return P_tilde
+            for j in range(0, self.num):
+                if i == j:
+                    A_Matrix[i, j] = self.A_ii(dip_i=i, dip_j=j)
+                if i != j:
+                    A_Matrix[i, j] = -self.A_ij(dip_i=i, dip_j=j)
+        return A_Matrix
+
+    def P_Mags(self, drive):
+        Einc_Vecs = np.tile(drive, (self.num, 1))
+        A_Matrix = self.A_Matrix()
+        B = np.sum(self.unit_vecs
+                   * Einc_Vecs, axis=1)[:, np.newaxis]
+        P_Mags = np.linalg.solve(A_Matrix, B)
+        return P_Mags
 
     def cross_sects(self, drive):
-        ''' Works for spheres up to 50 nm radii, and in the window < 3.0 eV '''
-        k = self.w/c*self.n
-        P = self.P_tilde(drive=drive)
-        P_each = np.zeros((self.num_radii_combs, self.num_gap, self.num_freq, 3, self.num),dtype=complex)
-        E_each = np.zeros((self.num_radii_combs, self.num_gap, self.num_freq, 3, self.num),dtype=complex)
-        Cext_each = np.zeros((self.num_radii_combs, self.num_gap, self.num_freq, self.num))
-        Cabs_each = np.zeros((self.num_radii_combs, self.num_gap, self.num_freq, self.num))
-        for i in range(0, self.num):
-            # Evaluate the cross sections of each particle separately
-            P_each[:, :, :, :,i] = P[:, :, :, 3*i:3*(i+1), 0]
-            E_each[:, :, :, 0,i] = self.dip_params.alpha(w=self.w, radius=self.all_radii[:, :, :, i])**(-1)*P_each[:, :, :, 0,i]
-            E_each[:, :, :, 1,i] = self.dip_params.alpha(w=self.w, radius=self.all_radii[:, :, :, i])**(-1)*P_each[:, :, :, 1,i]
-            E_each[:, :, :, 2,i] = self.dip_params.alpha(w=self.w, radius=self.all_radii[:, :, :, i])**(-1)*P_each[:, :, :, 2,i]
-
-            Cext_each[:, :, :, i] = 4*np.pi*k*np.imag( np.sum( P_each[:, :, :,:, i]*np.conj(E_each[:,:, :,:, i]),axis=3 )) *10**8
-            Cabs_each[:, :, :, i] = Cext_each[:, :, :, i] - 4*np.pi*k*2/3*k**3*np.real( np.sum(P_each[:, :, :,:, i]*np.conj(P_each[:,:, :,:, i]), axis=3) ) *10**8  
-
-        return Cext_each, Cabs_each
-
-
+        P = (self.P_Mags(drive=drive) * self.unit_vecs)
+        # print(self.alphas.reshape(1, self.num).shape, P.shape)
+        Cext = np.zeros((self.num))
+        for i in range(self.num):
+            E = self.alphas[i]**(-1) * P[i, :]
+            Cext[i] = ((4 * np.pi * self.k
+                        * np.imag(np.dot(P[i, :], np.conj(E))) * 10**8))
+        # Cabs_ea = (Cext_ea - 4 * np.pi * self.k * 2 / 3 * self.k**3
+        #            * np.real(np.sum(P_ea * np.conj(P_ea), axis=2)) * 10**8)
+        return Cext  # , Cabs_ea
